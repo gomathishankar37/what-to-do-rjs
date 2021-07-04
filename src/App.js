9f3fb9262f3ebbc56ff05035fda384894929814b
Gomathi Shankar <60147019+gomathishankar37@users.noreply.github.com>
2021-07-04 21:03:44 +0530

    Refactored with Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 const App = () => {
   const [todo, setTodo] = useState("");
@@ -44,26 +46,18 @@ const App = () => {
     <div className="App">
       <div className="container">
         <h1> What To Do </h1>
-        <form className="todoform" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={todo}
-            onChange={(e) => setTodo(e.target.value)}
-          />
-          <button type="submit"> {editId ? "Edit" : "Add"} </button>
-        </form>
+        <TodoForm
+          handleSubmit={handleSubmit}
+          todo={todo}
+          setTodo={setTodo}
+          editId={editId}
+        />
 
-        <ul className="allTodos">
-          {todos.map((t) => (
-            <li className="singleTodo">
-              <span className="todoText" key={t.id}>
-                {t.todo}
-              </span>
-              <button onClick={() => handleEdit(t.id)}> Edit </button>
-              <button onClick={() => handleDelete(t.id)}> Delete </button>
-            </li>
-          ))}
-        </ul>
+        <TodoList
+          todos={todos}
+          handleEdit={handleEdit}
+          handleDelete={handleDelete}
+        />
       </div>
     </div>
   );
