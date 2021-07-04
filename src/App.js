a03e7654a96ff7039c3c75160b228d40c011dd1f
Gomathi Shankar <60147019+gomathishankar37@users.noreply.github.com>
2021-07-04 20:49:07 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editId, setEditId] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (editId) {
+      const editTodo = todos.find((i) => i.id == editId);
+      const updatedTodos = todos.map((t) =>
+        t.id === editTodo.id
+          ? (t = { id: t.id, todo })
+          : { id: t.id, todo: t.todo }
+      );
+      setTodos(updatedTodos);
+      setEditId(0);
+      setTodo("");
+      return;
+    }
+
+    console.log(setTodos);
+    if (todo !== "") {
+      setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos]);
+      setTodo("");
+    }
+  };
+
+  const handleEdit = (id) => {
+    const editTodo = todos.find((i) => i.id === id);
+    setTodo(editTodo.todo);
+    setEditId(id);
+  };
+
+  const handleDelete = (id) => {
+    const delTodo = todos.filter((to) => to.id !== id);
+    setTodos([...delTodo]);
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1> What To Do </h1>
+        <form className="todoform" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button type="submit"> {editId ? "Edit" : "Add"} </button>
+        </form>
+
+        <ul className="allTodos">
+          {todos.map((t) => (
+            <li className="singleTodo">
+              <span className="todoText" key={t.id}>
+                {t.todo}
+              </span>
+              <button onClick={() => handleEdit(t.id)}> Edit </button>
+              <button onClick={() => handleDelete(t.id)}> Delete </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
